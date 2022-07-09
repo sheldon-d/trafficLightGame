fb0985f5135c5a23aa2304b6340458faebbf0f01
Sheldon <sheldondesa@hotmail.com>
2022-07-09 12:16:06 +1200

    Get random numbers and words of particular lengths

---
+++ phrases.py
@@ -0,0 +1,13 @@
+from random import randint, sample
+from english_words import english_words_alpha_set
+
+def get_random_number(num_chars):
+    return "".join([str(randint(0, 9)) for _ in range(num_chars)])
+
+def get_random_word(num_chars):
+    word = ""
+
+    while word == "":
+        sample_words = sample(english_words_alpha_set, 100)     # Randomly sample 100 words
+        word = next((x for x in sample_words if len(x) == num_chars and x[0].islower()), "")
+    return word
